d97cba25a1a9357d05950248ace16f7f3f2ba549
Elisa Mamolo <elisa.mamolo@gmail.com>
2019-07-16 08:08:31 +0000

    added events on image and button click to show message toast

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,6 +6,13 @@ sap.ui.define([
 	return Controller.extend("opensap.movies.controller.App", {
 		onInit: function () {
 
+		},
+		onPress: function (sValue) {
+		//load message toast asynchronously because don't know if the user will push the btn
+		//improve initial loading time
+		sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
+		oMessage.show("Searching..." + " " + sValue);
+		});
 		}
 	});
 });
