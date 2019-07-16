3f3977f2698028f409601a0652f9018d333ad291
Elisa Mamolo <elisa.mamolo@gmail.com>
2019-07-16 08:29:37 +0000

    add Log.info to show when the view and controller are rendered and initialized

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,18 +1,33 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/base/Log"
+], function (Controller, Log) {
 	"use strict";
 
 	return Controller.extend("opensap.movies.controller.App", {
 		onInit: function () {
-
+			Log.info("Controller has been initialized!");
 		},
+		
+		onBeforeRendering: function () {
+			Log.info("The view will be shortly rendered!");
+		},
+		
+		onAfterRendering: function () {
+			Log.info("The view has been rendered");
+		},
+		
 		onPress: function (sValue) {
 		//load message toast asynchronously because don't know if the user will push the btn
 		//improve initial loading time
 		sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
 		oMessage.show("Searching..." + " " + sValue);
 		});
+		},
+		
+		onExit: function (){
+			Log.info("Controller has been destroyed");
 		}
+		
 	});
 });
